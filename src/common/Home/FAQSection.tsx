e9d0ed59996c37de5b430946d71d656897426c4c
Marx Alonso <marxchip99@gmail.com>
2025-06-07 21:57:02 -0500

    remodelo de ts en faqs

--- src/common/Home/FAQSection.tsx
+++ src/common/Home/FAQSection.tsx
@@ -2,25 +2,8 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { ChevronDown } from "lucide-react";
 import { driver } from "driver.js";
+import {faqs} from "./faqs"
 import "driver.js/dist/driver.css";
-const faqs = [
-  {
-    question: "¿Puedo usar los diseños en proyectos personales o comerciales?",
-    answer: "Sí, puedes utilizar los módulos libremente en tus proyectos personales o comerciales. Agradecemos el reconocimiento si los compartes."
-  },
-  {
-    question: "¿Están optimizados para dispositivos móviles?",
-    answer: "Sí, todos los diseños están hechos con enfoque responsive utilizando TailwindCSS."
-  },
-  {
-    question: "¿Necesito conocimientos avanzados en CSS para usarlos?",
-    answer: "No necesariamente. Los módulos están pensados para ser reutilizables fácilmente, pero tener conocimientos básicos de HTML y CSS ayuda."
-  },
-  {
-    question: "¿Puedo contribuir con mis propios diseños?",
-    answer: "¡Claro! Estamos construyendo una comunidad para que más desarrolladores compartan sus componentes y diseños."
-  }
-];
 
 export const FAQSection = () => {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
---
+++ src/common/Home/faqs.ts
@@ -0,0 +1,18 @@
+export const faqs = [
+    {
+      question: "¿Puedo usar los diseños en proyectos personales o comerciales?",
+      answer: "Sí, puedes utilizar los módulos libremente en tus proyectos personales o comerciales. Agradecemos el reconocimiento si los compartes."
+    },
+    {
+      question: "¿Están optimizados para dispositivos móviles?",
+      answer: "Sí, todos los diseños están hechos con enfoque responsive utilizando TailwindCSS."
+    },
+    {
+      question: "¿Necesito conocimientos avanzados en CSS para usarlos?",
+      answer: "No necesariamente. Los módulos están pensados para ser reutilizables fácilmente, pero tener conocimientos básicos de HTML y CSS ayuda."
+    },
+    {
+      question: "¿Puedo contribuir con mis propios diseños?",
+      answer: "¡Claro! Estamos construyendo una comunidad para que más desarrolladores compartan sus componentes y diseños."
+    }
+  ];
